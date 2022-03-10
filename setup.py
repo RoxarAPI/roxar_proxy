b29d03836c9f4c9a5ce64a72846cea50911fa626
Havard Bjerke <havard.bjerke@emerson.com>
2022-03-10 10:29:19 +0100

    Created roxar_proxy package.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="roxar_proxy",
+    version="0.0.1",
+    author="Havard Bjerke",
+    author_email="havard.bjerke@emerson.com",
+    description="Roxar mock library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RoxarAPI/roxar_proxy",
+    project_urls={
+        "Bug Tracker": "https://github.com/RoxarAPI/roxar_proxy/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+    packages=["roxar_proxy"],
+    python_requires=">=3.7",
+)
+
